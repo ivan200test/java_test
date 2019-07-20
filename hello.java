80262a635587a087b6231e3e3fa9e935161e5acf
Ivan Test2000 <ivan200test@yandex.ru>
2019-07-20 13:53:20 +0400

    dobavleno znacenie x < 10

--- hello.java
+++ hello.java
@@ -1,9 +1,14 @@
 public class hello {
 	public static void main (String[] args) {
+		int x = 1;
 		System.out.println ("Helloy");
 		System.out.println ("Helloy" + " " + "World");
 		System.out.println ("Helloy" + " " + "World" + " ! ");
-		
+		while (x < 10) {
+			System.out.println ("Cislo");
+			x = x + 1;
+			System.out.println ("ravno " + x);
+		}
 	}
 
 }
